140b82fa3adca59e4167ffb00f43a9e9595e45d0
Unknown <darrens@hotmail.fr>
2018-03-19 17:51:12 +0100

    Add graph bac

---
+++ visu/script/bac.js
@@ -0,0 +1,114 @@
+function bac(discipline){
+    d3.csv("data/insertion_lp.csv", function(error,data){
+        var bacS;
+        var bacL;
+        var bacES;
+        var bacDispense;
+        var bacAutre;
+
+        data.forEach(function (d){
+            if(d.discipline == discipline){
+                bacS = d.bacS;
+                bacL = d.bacL;
+                bacES = d.bacES;
+                bacDispense = d.bac_dispense;
+                bacAutre = d.bac_autre;
+            }
+        })
+        
+        var pie = new d3pie("bac", {
+            "header": {
+                "title": {
+                    "text": "Lots of Programming Languages",
+                    "fontSize": 24,
+                    "font": "open sans"
+                },
+                "subtitle": {
+                    "text": "A full pie chart to show off label collision detection and resolution.",
+                    "color": "#999999",
+                    "fontSize": 12,
+                    "font": "open sans"
+                },
+                "titleSubtitlePadding": 9
+            },
+            "footer": {
+                "color": "#999999",
+                "fontSize": 10,
+                "font": "open sans",
+                "location": "bottom-left"
+            },
+            "size": {
+                "canvasWidth": 590,
+                "pieOuterRadius": "90%"
+            },
+            "data": {
+                "sortOrder": "value-desc",
+                "content": [
+                    {
+                        "label": "Bac S",
+                        "value": parseFloat(bacS),
+                        "color": "#0c6197"
+                    },
+                    {
+                        "label": "Bac L",
+                        "value": parseFloat(bacL),
+                        "color": "#4daa4b"
+                    },
+                    {
+                        "label": "Bac ES",
+                        "value": parseFloat(bacES),
+                        "color": "#e98125"
+                    },
+                    {
+                        "label": "Autre BAC",
+                        "value": parseFloat(bacAutre),
+                        "color": "#830909"
+                    },
+                    {
+                        "label": "Sans BAC",
+                        "value": parseFloat(bacDispense),
+                        "color": "#ce2aeb"
+                    }
+                ]
+            },
+            "labels": {
+                "outer": {
+                    "pieDistance": 32
+                },
+                "inner": {
+                    "hideWhenLessThanPercentage": 3
+                },
+                "mainLabel": {
+                    "fontSize": 11
+                },
+                "percentage": {
+                    "color": "#ffffff",
+                    "decimalPlaces": 0
+                },
+                "value": {
+                    "color": "#adadad",
+                    "fontSize": 11
+                },
+                "lines": {
+                    "enabled": true
+                },
+                "truncation": {
+                    "enabled": true
+                }
+            },
+            "effects": {
+                "pullOutSegmentOnClick": {
+                    "effect": "linear",
+                    "speed": 400,
+                    "size": 8
+                }
+            },
+            "misc": {
+                "gradient": {
+                    "enabled": true,
+                    "percentage": 100
+                }
+            }
+        })
+    })
+}
