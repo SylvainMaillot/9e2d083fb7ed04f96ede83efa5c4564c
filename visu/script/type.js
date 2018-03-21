3c116f4da399059e1dcdcd2fb2e659cfbf2080c4
Unknown <darrens@hotmail.fr>
2018-03-21 14:55:15 +0100

    Added type graph

---
+++ visu/script/type.js
@@ -0,0 +1,98 @@
+function type(discipline){
+	d4.csv("data/insertion_lp.csv", function(error,data){
+        var CDI;
+        var CDD;
+        var Fonctionnaires;
+        var Autres;
+
+        data.forEach(function (d){
+            if(d.discipline == discipline){
+                CDI = d.CDI;
+                CDD = d.CDD;
+                Fonctionnaires = d.fonctonnaire;
+                Autres = d.contrat_autre;
+            }
+        })
+    var pie = new d4pie("type", {
+	"header": {
+		"title": {
+			"text": "Types de contrat",
+			"fontSize": 24,
+			"font": "Arial"
+		},
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 10,
+		"font": "open sans",
+		"location": "bottom-left"
+	},
+	"size": {
+		"canvasWidth": 590,
+		"pieInnerRadius": "80%",
+		"pieOuterRadius": "100%"
+	},
+	"data": {
+		"sortOrder": "value-desc",
+		"content": [
+			{
+				"label": "CDI",
+				"value": parseFloat(CDI),
+				"color": "#003F57"
+			},
+			{
+				"label": "CDD",
+				"value": parseFloat(CDD),
+				"color": "#96D0DE"
+			},
+			{
+				"label": "Fonctionnaire",
+				"value": parseFloat(Fonctionnaires),
+				"color": "#E34824"
+			},
+			{
+				"label": "Autres",
+				"value": parseFloat(Autres),
+				"color": "#BDD02B"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 32
+		},
+		"inner": {
+			"hideWhenLessThanPercentage": 3
+		},
+		"mainLabel": {
+			"fontSize": 11
+		},
+		"percentage": {
+			"color": "#ffffff",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#adadad",
+			"fontSize": 11
+		},
+		"lines": {
+			"enabled": true
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	},
+	"misc": {
+		"gradient": {
+			"enabled": true,
+			"percentage": 100
+		}
+	}
+})})}
